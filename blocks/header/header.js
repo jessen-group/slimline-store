4c9fae1d928ff58045efab1a1093fb086cf583d0
azeneth-g <Azeneth.g@slimlinewarehouse.com.au>
2026-06-25 08:46:00 +0800

    Fix logo 4

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -19,13 +19,13 @@ const overlay = document.createElement('div');
 overlay.classList.add('overlay');
 document.querySelector('header').insertAdjacentElement('afterbegin', overlay);
 
-const nav = document.getElementById('nav');
-const logo = nav.querySelector('.nav-brand img');
+// const nav = document.getElementById('nav');
+// const logo = nav.querySelector('.nav-brand img');
 
-if (logo) {
-  logo.src = '/icons/dline/logo-white.png';
-  logo.alt = 'Dline';
-}
+// if (logo) {
+//   logo.src = '/icons/dline/logo-white.png';
+//   logo.alt = 'Dline';
+// }
 
 function closeOnEscape(e) {
   if (e.code === 'Escape') {
